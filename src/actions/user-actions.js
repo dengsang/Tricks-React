596edc93b8774b820e34622ea665740839372bc2
dengsang <dengsangt@gmail.com>
2018-03-21 10:56:18 +0300

    export the various actions types in its context

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,3 +1,27 @@
 /**
  * Created by DENG on 3/16/2018.
  */
+import * as types from '../actions/action-types';
+
+export function getUsersSuccess(users) {
+    return {
+        type: types.GET_USERS_SUCCESS,
+        users
+    };
+}
+
+export function deleteUserSuccess(userId) {
+    return {
+        type: types.DELETE_USER_SUCCESS,
+        userId
+    };
+    
+}
+
+export function userProfileSuccess(userProfile) {
+    return {
+        type: types.USER_PROFILE_SUCCESS,
+        userProfile
+    };
+    
+}
