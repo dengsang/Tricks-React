bb957bb8278c2e07da34ecd9e1b0a2602ad68205
dengsang <dengsangt@gmail.com>
2018-03-21 11:24:40 +0300

    Add widget-list views to the components

--- src/components/views/widget-list.js
+++ src/components/views/widget-list.js
@@ -1,3 +1,25 @@
 /**
  * Created by DENG on 3/19/2018.
  */
+import React from 'react';
+import { Link } from 'react-router';
+
+//stateless functional components
+export default function (props) {
+    return (
+        <div className="data-list">
+            {props.widgets.map(widget =>{
+                return (
+                    <div key={widget.id} className="data-list-item">
+                        <div className="details">{widget.name}</div>
+                        <div className="controls">
+                            <button onClick={props.deleteWidget.bind(null, widget.id)} className="delete">Delete</button>
+                        </div>
+
+                    </div>
+                );
+            })}
+        </div>
+
+    );
+}
