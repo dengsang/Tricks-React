164b137078bda94ffd6f8b6fc13dd5e2144b6cdf
dengsang <dengsangt@gmail.com>
2018-03-21 11:32:59 +0300

    Add reducers to user-reducer js file

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,3 +1,34 @@
 /**
  * Created by DENG on 3/16/2018.
  */
+import * as types from '../actions/actions-types';
+import _ from 'lodash';
+
+const initialState = {
+    users: [],
+    userProfile: {
+        repos: []
+    }
+};
+
+const userReducer = function (state = initialState, action) {
+
+    switch(action.types){
+        case types.GET_USERS_SUCCESS:
+            return Object.assign({}, state, {users: action.users});
+
+        case types.DELETE_USER_SUCCESS:
+
+            //using lodash to create a new user array without the user to be removed
+            const  newUsers = _.filter(state.users, user => user.id != action.userid);
+            return Object.assign({}, state, {users: newUsers});
+
+        case  types.USER_PROFILE_SUCCESS:
+            return Object.assign({}, state, {userProfile: action.userProfile});
+    }
+
+    return state;
+
+};
+
+export  default userReducer;
