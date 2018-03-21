a50adf91911f55a172cb8f7434fbed806c087894
dengsang <dengsangt@gmail.com>
2018-03-21 11:15:11 +0300

    Add layouts search-layouts js file

--- src/components/layouts/search-layout.js
+++ src/components/layouts/search-layout.js
@@ -1,3 +1,23 @@
 /**
  * Created by DENG on 3/19/2018.
  */
+import React from 'react';
+import SearchFormContainer from '../containers/search-form-container';
+
+// Stateless Functional Components being used
+export default function(props) {
+  return (
+    <div className="search">
+      <header className="search-header">
+        {props.title}
+        <SearchFormContainer searchType={props.searchType} />
+      </header>
+      <div className="search-results">
+        {props.children}
+      </div>
+      <footer className="search-footer">
+        {props.totalResults} Results
+      </footer>
+    </div>
+    );
+}
