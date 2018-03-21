8d2a31ad2051a790cad7f23f88eef539d65d0e71
dengsang <dengsangt@gmail.com>
2018-03-21 11:30:23 +0300

    Add reducers index js file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,20 @@
 /**
  * Created by DENG on 3/16/2018.
  */
+import  { combineReducers } from  'redux';
+
+
+//Reducers
+
+import userReducer from './user-reducer';
+import widgetReducer from './widget-reducer';
+import searchLayoutReducer from './search-layout-reducer';
+
+//Combine Reducers
+
+var reducers = combineReducers({userState: userReducer,
+widgetState: widgetReducer,
+searchLayoutState: searchLayoutReducer
+});
+
+export default reducers;
