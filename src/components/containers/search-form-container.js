ccdc76149ad363feb5d7a990bb63372080741420
dengsang <dengsangt@gmail.com>
2018-03-21 11:01:30 +0300

    Add search form container js file

--- src/components/containers/search-form-container.js
+++ src/components/containers/search-form-container.js
@@ -1,3 +1,38 @@
 /**
  * Created by DENG on 3/16/2018.
  */
+import React from 'react';
+import * as userApi from '../../api/user-api';
+import * as widgetApi from '../../api/widget-api';
+import { loadSearchLayout } from '../../actions/search-layout-actions';
+import  searchForm from '../views/search-form';
+
+
+const SearchFormContainer = React.createClass({
+
+    search: function (event) {
+        event.preventDefault();
+        // By assigning a "child" ref to <SearchForm />, ww
+        // can use that reference to gain access to the
+        // .getQuery() method. See the code for
+        // <SearchForm /> to see how it returns a value
+
+        let  query = this.refs.child.getQuery();
+
+        if(this.props.searchType === 'users'){
+            userApi.searchUsers(query);
+        }else if (this.props.searchType === 'widgets'){
+            widgetApi.searchWidget(query);
+        }
+
+    },
+
+    render: function () {
+        return (
+            <SearchForm search={this.search} ref="child"/>
+        );
+        
+    }
+});
+
+export default SearchFormContainer;
